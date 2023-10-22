42f6307f24e6ceded29254a9d2d1930481f4e4b2
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-10-22 23:30:06 +0300

    2 sınıfı birbirinden ayıran doğruyu bulma işlemi perceptron algoritması ile gerçekleştirildi

--- MyForm.h
+++ MyForm.h
@@ -5,6 +5,7 @@
 #include <time.h>
 #include "Resource.h"
 #include "Process.h"
+#include "Perceptron.h"
 
 namespace ArtificialNeuralNetwork {
 
@@ -33,12 +34,15 @@ namespace ArtificialNeuralNetwork {
 			label5->Text = "";
 			label6->Text = "";
 			label7->Text = "";
-			pen = gcnew Pen(Color::Black, 3.0f);
+			pen = gcnew Pen(Color::Red, 3.0f);
 			instanceValue = classValue = totalInstanceSize = constTotalInstanceSize = classId = 0;
+			
 			
 		}
 
 		Int32 instanceValue;//her sýnýfa ait örnek sayýsý
+	private: System::Windows::Forms::Label^ label8;
+	public:
 		Pen^ pen;//sýnýftaki örneklerin picturebox'ta görünmesini saðlayacak olan kalem nesnesi.
 		Int32 classValue;//sýnýf sayýsý
 		Instance* instances;// ekrana týklanan noktalarý tutacak yani örneklerin dizisi.
@@ -74,6 +78,10 @@ namespace ArtificialNeuralNetwork {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::ToolStripMenuItem^ randomizeToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ exitToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ trainingToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ perceptronToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ deltaToolStripMenuItem;
 	private:
 		/// <summary>
 		///Gerekli tasarýmcý deðiþkeni.
@@ -92,6 +100,10 @@ namespace ArtificialNeuralNetwork {
 			this->processToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->ýnitializationToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->randomizeToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exitToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->trainingToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->perceptronToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->deltaToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -104,6 +116,7 @@ namespace ArtificialNeuralNetwork {
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->menuStrip1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
@@ -116,6 +129,7 @@ namespace ArtificialNeuralNetwork {
 			this->pictureBox1->Location = System::Drawing::Point(15, 50);
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->Size = System::Drawing::Size(500, 500);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::pictureBox1_Paint);
@@ -135,22 +149,56 @@ namespace ArtificialNeuralNetwork {
 			// 
 			this->processToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->ýnitializationToolStripMenuItem });
 			this->processToolStripMenuItem->Name = L"processToolStripMenuItem";
-			this->processToolStripMenuItem->Size = System::Drawing::Size(72, 24);
+			this->processToolStripMenuItem->Size = System::Drawing::Size(72, 26);
 			this->processToolStripMenuItem->Text = L"Process";
 			// 
 			// ýnitializationToolStripMenuItem
 			// 
-			this->ýnitializationToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->randomizeToolStripMenuItem });
+			this->ýnitializationToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->randomizeToolStripMenuItem,
+					this->exitToolStripMenuItem, this->trainingToolStripMenuItem
+			});
 			this->ýnitializationToolStripMenuItem->Name = L"ýnitializationToolStripMenuItem";
-			this->ýnitializationToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->ýnitializationToolStripMenuItem->Size = System::Drawing::Size(174, 26);
 			this->ýnitializationToolStripMenuItem->Text = L"Initialization";
 			// 
 			// randomizeToolStripMenuItem
 			// 
 			this->randomizeToolStripMenuItem->Name = L"randomizeToolStripMenuItem";
-			this->randomizeToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->randomizeToolStripMenuItem->Size = System::Drawing::Size(167, 26);
 			this->randomizeToolStripMenuItem->Text = L"Randomize";
 			this->randomizeToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::randomizeToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this->exitToolStripMenuItem->Name = L"exitToolStripMenuItem";
+			this->exitToolStripMenuItem->Size = System::Drawing::Size(167, 26);
+			this->exitToolStripMenuItem->Text = L"Exit";
+			this->exitToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::exitToolStripMenuItem_Click);
+			// 
+			// trainingToolStripMenuItem
+			// 
+			this->trainingToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->perceptronToolStripMenuItem,
+					this->deltaToolStripMenuItem
+			});
+			this->trainingToolStripMenuItem->Name = L"trainingToolStripMenuItem";
+			this->trainingToolStripMenuItem->Size = System::Drawing::Size(167, 26);
+			this->trainingToolStripMenuItem->Text = L"Training";
+			// 
+			// perceptronToolStripMenuItem
+			// 
+			this->perceptronToolStripMenuItem->Name = L"perceptronToolStripMenuItem";
+			this->perceptronToolStripMenuItem->Size = System::Drawing::Size(163, 26);
+			this->perceptronToolStripMenuItem->Text = L"Perceptron";
+			this->perceptronToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::perceptronToolStripMenuItem_Click);
+			// 
+			// deltaToolStripMenuItem
+			// 
+			this->deltaToolStripMenuItem->Name = L"deltaToolStripMenuItem";
+			this->deltaToolStripMenuItem->Size = System::Drawing::Size(163, 26);
+			this->deltaToolStripMenuItem->Text = L"Delta";
+			this->deltaToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::deltaToolStripMenuItem_Click);
 			// 
 			// treeView1
 			// 
@@ -169,7 +217,7 @@ namespace ArtificialNeuralNetwork {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(162)));
 			this->label1->ForeColor = System::Drawing::SystemColors::ControlText;
-			this->label1->Location = System::Drawing::Point(608, 50);
+			this->label1->Location = System::Drawing::Point(743, 50);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(154, 22);
 			this->label1->TabIndex = 2;
@@ -183,7 +231,7 @@ namespace ArtificialNeuralNetwork {
 			this->label2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(162)));
-			this->label2->Location = System::Drawing::Point(608, 93);
+			this->label2->Location = System::Drawing::Point(743, 93);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(153, 22);
 			this->label2->TabIndex = 3;
@@ -191,7 +239,7 @@ namespace ArtificialNeuralNetwork {
 			// 
 			// numericUpDown1
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(799, 50);
+			this->numericUpDown1->Location = System::Drawing::Point(934, 50);
 			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
 			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 2, 0, 0, 0 });
 			this->numericUpDown1->Name = L"numericUpDown1";
@@ -201,7 +249,7 @@ namespace ArtificialNeuralNetwork {
 			// 
 			// numericUpDown2
 			// 
-			this->numericUpDown2->Location = System::Drawing::Point(799, 92);
+			this->numericUpDown2->Location = System::Drawing::Point(934, 92);
 			this->numericUpDown2->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
 			this->numericUpDown2->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 2, 0, 0, 0 });
 			this->numericUpDown2->Name = L"numericUpDown2";
@@ -214,7 +262,7 @@ namespace ArtificialNeuralNetwork {
 			this->button1->BackColor = System::Drawing::Color::Khaki;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(162)));
-			this->button1->Location = System::Drawing::Point(608, 136);
+			this->button1->Location = System::Drawing::Point(743, 136);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(311, 36);
 			this->button1->TabIndex = 6;
@@ -227,7 +275,7 @@ namespace ArtificialNeuralNetwork {
 			this->button2->BackColor = System::Drawing::Color::Khaki;
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(162)));
-			this->button2->Location = System::Drawing::Point(609, 178);
+			this->button2->Location = System::Drawing::Point(744, 178);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(310, 36);
 			this->button2->TabIndex = 7;
@@ -238,7 +286,7 @@ namespace ArtificialNeuralNetwork {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(606, 248);
+			this->label3->Location = System::Drawing::Point(741, 248);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(100, 17);
 			this->label3->TabIndex = 8;
@@ -247,7 +295,7 @@ namespace ArtificialNeuralNetwork {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(608, 279);
+			this->label4->Location = System::Drawing::Point(743, 279);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(287, 17);
 			this->label4->TabIndex = 9;
@@ -256,7 +304,7 @@ namespace ArtificialNeuralNetwork {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(990, 50);
+			this->label5->Location = System::Drawing::Point(1125, 50);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(46, 17);
 			this->label5->TabIndex = 10;
@@ -265,7 +313,7 @@ namespace ArtificialNeuralNetwork {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(990, 77);
+			this->label6->Location = System::Drawing::Point(1125, 77);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(46, 17);
 			this->label6->TabIndex = 11;
@@ -274,11 +322,20 @@ namespace ArtificialNeuralNetwork {
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(990, 98);
+			this->label7->Location = System::Drawing::Point(1125, 98);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(46, 17);
 			this->label7->TabIndex = 12;
 			this->label7->Text = L"label7";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(744, 317);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(143, 17);
+			this->label8->TabIndex = 13;
+			this->label8->Text = L"Total Cycle Count = 0";
 			// 
 			// MyForm
 			// 
@@ -286,6 +343,7 @@ namespace ArtificialNeuralNetwork {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FloralWhite;
 			this->ClientSize = System::Drawing::Size(1303, 603);
+			this->Controls->Add(this->label8);
 			this->Controls->Add(this->label7);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
@@ -356,7 +414,7 @@ namespace ArtificialNeuralNetwork {
 			}
 			else {
 				classValue--;
-				classId++;
+				classId = classId + 2;
 				instanceValue = Decimal::ToInt32(numericUpDown2->Value);
 				System::Random^ rastgele = gcnew System::Random();
 				pen = gcnew Pen(System::Drawing::Color::FromArgb(rastgele->Next(256), rastgele->Next(256), rastgele->Next(256)), 3.0f);
@@ -384,7 +442,7 @@ namespace ArtificialNeuralNetwork {
 		totalInstanceSize = classValue * instanceValue;//toplam örnek sayýsý
 		constTotalInstanceSize = classValue * instanceValue;//toplam örnek sayýsý ama bu sabit kalacak
 
-		classId = 1;//sýnýf id'si 1 den baþlayacak
+		classId = -1;//sýnýf id'si 1 den baþlayacak
 		instances = new Instance[totalInstanceSize];//bellekte toplam örnek sayýsý kadar alan ayrýlacak.
 		
 		label3->Text = "x1 = 0  "  + "  x2 = 0" ;
@@ -396,24 +454,22 @@ namespace ArtificialNeuralNetwork {
 	}
 
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		
-	/*	numericUpDown1->Enabled = true;
+	
+		delete[] instances;
+		pictureBox1->Refresh();
+		classValue = 0;
+		instanceValue = 0;
+		totalInstanceSize = 0;
+		constTotalInstanceSize = 0;
+		classId = 0;
+		numericUpDown1->Enabled = true;
 		numericUpDown2->Enabled = true;
 
-		classValue = 2;
-		instanceValue = 2;
-
-		pen = gcnew Pen(Color::Black, 3.0f);
-		instanceValue = classValue = totalInstanceSize = constTotalInstanceSize = classId = 0;
-		delete* instances*/
-
-		MessageBox::Show("class" + instances[0].id + "  x1:" + instances[0].x1 + " x2:" + instances[0].x2 + "\n" +
-			"class" + instances[1].id + "  x1:" + instances[1].x1 + " x2:" + instances[1].x2 + "\n" +
-			"class" + instances[2].id + "  x1:" + instances[2].x1 + " x2:" + instances[2].x2 + "\n" +
-			"class" + instances[3].id + "  x1:" + instances[3].x1 + " x2:" + instances[3].x2 + "\n" +
-			"class" + instances[4].id + "  x1:" + instances[4].x1 + " x2:" + instances[4].x2 + "\n");
-		
-	
+		label3->Text = "x1 = 0  " + "  x2 = 0";
+		label4->Text = "Eklenebilir örnek adedi: 0 " + " Class Id: Unkown";
+		label5->Text = "w[0]: 0 " ;
+		label6->Text = "w[1]: 0 " ;
+		label7->Text = "w[2]: 0 " ;
 		
 	}
 	private: System::Void randomizeToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
@@ -440,6 +496,101 @@ namespace ArtificialNeuralNetwork {
 		max_y = YPoint(max_x, weight);
 		pictureBox1->CreateGraphics()->DrawLine(pen, (pictureBox1->Width / 2) + min_x, (pictureBox1->Height / 2) - min_y, (pictureBox1->Width / 2) + max_x, (pictureBox1->Height / 2) - max_y);
 
+	}
+	private: System::Void exitToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		//exit form
+	}
+
+
+	private: System::Void perceptronToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		//perceptron learning
+
+		//double totalError = 0.0;
+
+		//int totalCycleCount = 0;
+
+		//double tolerance = 1e-9; // Tolerans deðeri
+
+		//while (true) {
+		//	for (int i = 0; i < constTotalInstanceSize; i++) {
+
+		//		totalError += perceptronLearning(instances[i], weight, 0.1);
+
+
+		//	}
+		//	totalCycleCount++;
+
+		//	if (std::abs(totalError - 0) < tolerance) {
+		//		break;
+		//	}
+		//	else {
+		//		totalError = 0.0;
+		//	}
+		//}
+
+		//label8->Text = "Total Cycle Count = " + System::Convert::ToString(totalCycleCount);
+		//
+		//Pen^ pen = gcnew Pen(Color::Purple, 3.0f);
+
+		//int min_x, max_x, min_y, max_y;
+
+		//label5->Text = "w[0]: " + System::Convert::ToString(weight[0]);
+		//label6->Text = "w[1]: " + System::Convert::ToString(weight[1]);
+		//label7->Text = "w[2]: " + System::Convert::ToString(weight[2]);
+
+		//min_x = (this->pictureBox1->Width) / -2;
+		//min_y = YPoint(min_x, weight);
+		//max_x = (this->pictureBox1->Width) / 2;
+		//max_y = YPoint(max_x, weight);
+		//pictureBox1->CreateGraphics()->DrawLine(pen, (pictureBox1->Width / 2) + min_x, (pictureBox1->Height / 2) - min_y, (pictureBox1->Width / 2) + max_x, (pictureBox1->Height / 2) - max_y);
+		//
+
+
+		Pen^ pen = gcnew Pen(Color::Purple, 3.0f);
+
+		int min_x, max_x, min_y, max_y;
+
+		int cycle = 10000;
+		bool allCyclesDone;
+		int totalCycleCount = 0;
+		for (int i = 0; i < cycle; i++) {
+			totalCycleCount++;
+			bool* trainingIsDone = new bool[constTotalInstanceSize];
+
+			allCyclesDone = true; //Butun cycle'lar tamamlandimi
+
+			for (int j = 0; j < constTotalInstanceSize; j++) {
+				trainingIsDone[j] = perceptronLearning(instances[j], weight, 0.1);
+			}
+				
+			for (int k = 0; k < constTotalInstanceSize; k++)
+				if (trainingIsDone[k] == 0)
+					allCyclesDone = false;
+
+			if (allCyclesDone) {
+				delete[] trainingIsDone;
+				break;
+			}
+				
+		}
+
+		label5->Text = "w[0]: " + System::Convert::ToString(weight[0]);
+		label6->Text = "w[1]: " + System::Convert::ToString(weight[1]);
+		label7->Text = "w[2]: " + System::Convert::ToString(weight[2]);
+		label8->Text = "Total Cycle Count = " + System::Convert::ToString(totalCycleCount);
+
+		min_x = (this->pictureBox1->Width) / -2;
+		min_y = YPoint(min_x, weight);
+		max_x = (this->pictureBox1->Width) / 2;
+		max_y = YPoint(max_x, weight);
+
+		pen->Color = Color::Purple;
+		pictureBox1->CreateGraphics()->DrawLine(pen, (pictureBox1->Width / 2) + min_x, (pictureBox1->Height / 2) - min_y, (pictureBox1->Width / 2) + max_x, (pictureBox1->Height / 2) - max_y);
+
+		
+	}
+	private: System::Void deltaToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		//delta learning
 	}
 };
 }
---
+++ Perceptron.cpp
@@ -0,0 +1,68 @@
+#include "Perceptron.h"
+#include "Process.h"
+
+
+template <typename T> int sgn(T val) {
+	return (T(0) < val) - (val < T(0));
+}
+
+//
+//       w1
+//   x1 -----------
+//             -------
+//       w2         -----
+//   x2 --------------------- net------sgn(net) -1 or +1
+//                  -----
+//       w3     -----
+//   x3=1 ----------
+//
+
+//double perceptronLearning(Instance instance, double* weights, double c ) {//bu fonksiyon her instance için bir hata deðeri döndürür.
+//	//tek nöronlu að c öðrenme sabiti 0.1
+//	// w1,w2,w3
+//	//bias x3 deðeri genelde 1 e eþit, net=W*x, output = f(net)
+//	double bias = 1, net = 0.0, output = 0.0;
+//	double desiredOutput = (int)instance.id;
+//
+//	// net = w[0]*x1 + w[1].x2 + w[2]*bias
+//	//her bir örnek için net hesaplansýn.
+//	net = instance.x1 * weights[0] + instance.x2 * weights[1] + bias * weights[2];
+//
+//	output = sgn(net);// çýkýþ -1 veya + 1 olur.
+//
+//	if (output != desiredOutput) {//örneðin beklenen çýkýþý ile aðýn ürettiði çýkýþ karþýlaþtýrýlsýn.eþit deðilse aðýrlýklar güncellensin.
+//		weights[0] = weights[0] + c * (desiredOutput - output) * instance.x1;
+//		weights[1] = weights[1] + c * (desiredOutput - output) * instance.x2;
+//		weights[2] = weights[2] + c * (desiredOutput - output) * bias;
+//
+//		return desiredOutput - output;;//aðýrlýklar eþit deðil ise hata = desiredOutput - output
+//	}
+//	else {//eþit ise aðýrlýklar güncellenmez.
+//		return 0; //aðýrlýklar eþit deðilse hata = 0
+//	}
+//}
+
+bool perceptronLearning(Instance instance, double* weights, double c) {//bu fonksiyon her instance için bir hata deðeri döndürür.
+	//tek nöronlu að c öðrenme sabiti 0.1
+	// w1,w2,w3
+	//bias x3 deðeri genelde 1 e eþit, net=W*x, output = f(net)
+	double bias = 1, net = 0.0, output = 0.0;
+	double desiredOutput = (int)instance.id;
+
+	// net = w[0]*x1 + w[1].x2 + w[2]*bias
+	//her bir örnek için net hesaplansýn.
+	net = instance.x1 * weights[0] + instance.x2 * weights[1] + bias * weights[2];
+
+	output = sgn(net);// çýkýþ -1 veya + 1 olur.
+
+	if (output != desiredOutput) {//örneðin beklenen çýkýþý ile aðýn ürettiði çýkýþ karþýlaþtýrýlsýn.eþit deðilse aðýrlýklar güncellensin.
+		weights[0] = weights[0] + c * (desiredOutput - output) * instance.x1;
+		weights[1] = weights[1] + c * (desiredOutput - output) * instance.x2;
+		weights[2] = weights[2] + c * (desiredOutput - output) * bias;
+
+		return false;//aðýrlýklar eþit deðil ise hata = desiredOutput - output
+	}
+	else {//eþit ise aðýrlýklar güncellenmez.
+		return true; //aðýrlýklar eþit deðilse hata = 0
+	}
+}
